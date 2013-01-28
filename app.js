52b0fdf99c6736cf2d6140eb8d3699bb4df12e57
djyde <djyde520@gmail.com>
2013-01-28 17:29:53 +0800

    Submit and Login

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
@@ -28,7 +27,8 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/submit',routes.submit);
+app.get('/login',routes.login);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,11 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.submit = function(req,res){
+  res.render('submit');
+};
+
+exports.login = function(req,res){
+  res.render('login');
+}
